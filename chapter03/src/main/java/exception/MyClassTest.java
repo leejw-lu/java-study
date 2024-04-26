6e48ee6cf1367e711c5d8fcf35ac14066ce7c6f4
leejw-lu <jiwoojenny@naver.com>
2024-04-26 11:37:09 +0900

    수업: exception

---
+++ chapter03/src/main/java/exception/MyClassTest.java
@@ -0,0 +1,21 @@
+package exception;
+
+import java.io.IOException;
+
+public class MyClassTest {
+
+	public static void main(String[] args) {
+		try {
+			new MyClass().danger();
+		} catch (IOException e) {
+			//System.out.println("error: "+e);
+			e.printStackTrace();
+		}
+		catch (MyException e) {
+			//System.out.println("error: "+e);
+			System.out.println("error: "+e);
+		}
+		
+	}
+
+}
