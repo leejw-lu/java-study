c3057fef340191212ae2a780238be33a46d7a612
leejw-lu <jiwoojenny@naver.com>
2024-05-02 10:44:32 +0900

    과제: NSLookup

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,17 +2,29 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
 		try {
 			//스캐너 써저 www.naver.com 입력받기
-			InetAddress[] inetAddresses= InetAddress.getAllByName("www.naver.com");
+			Scanner scanner = new Scanner(System.in);
+			while (true) { 
+				System.out.print(">");
+				String line = scanner.nextLine();
+				
+				if (line.equals("exit")) {
+					break;
+				}
+				InetAddress[] inetAddresses= InetAddress.getAllByName(line);  //"www.naver.com"
 
-			for (InetAddress inetAddresse : inetAddresses) {
-				System.out.println(inetAddresse);
+				for (InetAddress inetAddresse : inetAddresses) {
+					System.out.println(inetAddresse);
+				}
+
 			}
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
